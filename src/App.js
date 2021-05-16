9dd1a4401f2c89241a4c0b478b8fe18946b9751c
Rahul-Devloper <rahulramesh26@yahoo.com>
2021-05-17 01:58:23 +0530

    learnt Switch

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './App.css';
+import About from './components/About';
+import Shop from './components/Shop';
+import Nav from './components/Nav';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+     <Nav />
+     <Switch>
+     <Route path = '/' exact component={Home} />
+     <Route path = '/about' component={About} />
+     <Route path = '/shop' component={Shop} />
+     </Switch>     
+    </div>
+    </Router>
+  );
+}
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+)
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <div>
+            <nav>
+                <h3>Logo</h3>
+                <ul className='nav-link'>
+                    <li>About</li>
+                    <li>Shop</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Nav
