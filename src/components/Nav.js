d7a63ef24287cf5198f4e0a46c8e7e7bfad56e87
Rahul-Devloper <rahulramesh26@yahoo.com>
2021-05-17 14:15:18 +0530

    fetched api

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,27 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom';
 const Nav = () => {
+
+    const navStyle = {
+        color: 'white',
+        textTransform: 'uppercase'
+    };
+
     return (
         <div>
             <nav>
+                <Link style= {navStyle} to = '/'>
                 <h3>Logo</h3>
+                </Link>
+                
                 <ul className='nav-link'>
-                    <li>About</li>
+                   <Link style= {navStyle} to = '/about'>
+                   <li>About</li>
+                   </Link> 
+                    <Link style= {navStyle} to = '/shop'>
                     <li>Shop</li>
+                    </Link>
+                    
                 </ul>
             </nav>
         </div>
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
 
 const Shop = () => {
+    
+    useEffect(()=>{
+        fetchItems();
+    },[]);
+
+    const [items, setItems] = useState([]);
+
+    
+    const fetchItems = async ()=>{
+        const data = await fetch(
+            'https://pokeapi.co/api/v2/ability/'
+            );
+
+            const items = await data.json();
+            console.log(items.name);
+            setItems(items.items);
+    };
+
+    
+
     return (
         <div>
-            <h1>Shop</h1>
+           {items.map(item =>(
+               <h1>{item.name}</h1>
+           ))}
         </div>
-    )
+    );
 }
 
 export default Shop
